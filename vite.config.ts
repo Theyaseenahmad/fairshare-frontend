75cf6068b68341bdc1002037e128ba5bd261d564
Yaseen Ahmad <yaseenahmad02.777@gmail.com>
2025-03-14 14:18:42 +0530

    tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fairshare-backend-bj8e.onrender.com',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
+});
